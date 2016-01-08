70623e5d250f14fd710d0137802b180cb175559e
Apple <Eloy@apples-macbook-air.local>
2016-01-08 11:07:34 -0600

    Initial Commit

---
+++ iTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iTest
+//
+//  Created by Eloy Rico on 08/01/16.
+//  Copyright © 2016 CR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
